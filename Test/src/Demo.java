2e0c4bfb7b0f20b58927345b21cc4bfce7f6b58e
wangjieUser <313084443@qq.com>
2018-11-26 11:00:01 +0800

    111

---
+++ Test/src/Demo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+		list.add("11");
+		System.out.println(list);
+		System.out.println(list.toString());
+	}
+
+}
